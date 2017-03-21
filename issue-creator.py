2fb806b6f9628aeb191ba2bef696934d9590d287
Becca James <james.becca@me.com>
2017-03-21 09:04:22 -0400

    Update issue-creator.py

--- issue-creator.py
+++ issue-creator.py
@@ -1,8 +1,6 @@
-## `source github/bin/activate` will open github virtualenv
-## using external library called github3.py
-## run `pip install github3.py` to install github library onto virtualenv
-## run `python Desktop/issue-creator.py` to point to this file's location and update file name if you changed it!
-## tip: begin lines with `clear &&` to clear the output of the previous command and run command cleanly
+## In terminal use `source github/bin/activate` to open github virtualenv
+## Run `pip install github3.py` to install github3.py library in virtualenv
+## Run `python LOCATION/FILENAME-TODO.py` to point to this file's location
 
 import csv
 import sys
@@ -18,16 +16,15 @@ repo = gh.repository("ORG-NAME-TODO","REPO-NAME-TODO")
 milestones = {}
 for milestone in repo.iter_milestones():
     milestones[milestone.title] = milestone.number
-## this bit creates a dictionary of all the milestones present in your repo and associates their titles with their integer in a key
+## this creates a dictionary of all the milestones present in your repo and associates their titles with their integer in a key
 
 with open('LOCATION/FILENAME-TODO.CSV') as f:
 ## include the location/name of your csv file within open()
-## ensure that your csv includes only the data you want to pull in and create issues for (i.e. no headers!)
-## Becca should include a programmatic option to remove unnecessary rows
+## ensure that your csv includes only the data you want to pull in and create issues for (i.e. no spreadsheet column/row headers!)
     reader = csv.reader(f)
     for row in reader:
         print('Creating an issue titled: ' + row[1])
-## this tells you what your program is doing in your terminal
+## this allows you to see what your computer is working on when this script runs through your terminal
         labels=row[3].split(",")
 ## this allows you to use a comma-separated list surrounded by quotes to create a list of labels
         issue = repo.create_issue(title=row[1],body=row[2],labels=labels,assignee=row[4],milestone=(milestones[row[5]] if row[5] else None))
